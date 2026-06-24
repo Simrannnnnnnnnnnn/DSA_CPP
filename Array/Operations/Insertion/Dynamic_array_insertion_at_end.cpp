ede352d1f1e79e4c653da75e1a381a17562328a8
Simran Kaur <95494098+Simrannnnnnnnnnnn@users.noreply.github.com>
2026-06-24 23:07:09 +0530

    Add files via upload

---
+++ Array/Operations/Insertion/Dynamic_array_insertion_at_end.cpp
@@ -0,0 +1,42 @@
+/******************************************************************************
+                Array Basics
+                DYNAMIC ARRAY - Insertion at beginning 
+                Time Complexity o(n) 
+*******************************************************************************/
+#include <iostream>
+using namespace std;
+int main()
+{   int size; 
+    cout<<"Enter the size of array\n";
+    cin>>size;
+    
+    if (size<=0){
+        cout<<"sorry! size cant be zero or negative please enter positive size";
+        return 1;
+    }
+    int* arr = new int[size+1];
+    
+    cout<<"enter the "<<size<<" elements "<<endl;
+    for(int i =0; i<size; i++){
+        cin>>arr[i];
+    }
+    cout<<"Array elements after insertion are "<<endl;
+    for(int i =0; i<size; i++){
+        cout<<arr[i]<<" ";
+    }
+    
+    int n;
+    cout<<"enter the element to insert at the end"<<endl;
+    cin>>n;
+    
+    cout<<"array after insertion at end"<<endl;
+    arr[size]=n;
+    size++;
+   
+    for(int i =0; i < size; i++){
+        cout<<arr[i]<<" ";
+    }
+    
+    delete[] arr;
+    return 0;
+}
